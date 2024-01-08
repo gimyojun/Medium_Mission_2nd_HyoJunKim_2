7bba3a86e423df3c61eb212b13b69b9b8bf35e05
gimyojun <gywnsdl5623@gmail.com>
2024-01-09 04:54:30 +0900

    JwtAuthenticationFilter 에서 가짜 회원생성 후 강제 로그인 처리

--- src/main/java/com/ll/medium/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ll/medium/global/security/JwtAuthenticationFilter.java
@@ -4,8 +4,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -13,6 +19,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @SneakyThrows
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
         System.out.println("JwtAuthenticationFilter 실행");
+        User user = new User("user1", "", List.of());
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                user,
+                user.getPassword(),
+                user.getAuthorities()
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
 
         filterChain.doFilter(request, response);
     }
